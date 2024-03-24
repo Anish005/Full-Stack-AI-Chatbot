cc78c36db8dd95a15358e9705a3bba7dd3c909bc
Anish Panda <anishpanda31@gmail.com>
2024-03-24 15:45:29 +0530

    setting up routes and controllers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
 //application code to be handled under app.ts
 import express from 'express'
 import {config} from "dotenv"
+import morgan from "morgan"
+import appRouter from './routes/index.js';
+
 
 config();
 const app  = express()
@@ -8,5 +11,10 @@ const app  = express()
 
 //middlewares
 app.use(express.json());
+
+//remove it in production
+app.use(morgan("dev"))
+
+app.use("api/v1",appRouter)
 
 export default app;
