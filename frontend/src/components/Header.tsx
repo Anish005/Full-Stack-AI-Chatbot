6375f8b16530c4fb2a0ed40c5e5bac57d7bfb40a
Anish Panda <anishpanda31@gmail.com>
2024-03-25 15:23:17 +0530

    creating the Logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import Appbar from '@mui/material/AppBar'
+import  Toolbar  from '@mui/material/Toolbar'
+import Logo from './shared/Logo';
 const Header = () => {
   return (
-    <div>Header</div>
+    <Appbar sx={{bgcolor:"transparent", position:"static", boxShadow:"none"}}>
+        <Toolbar sx={{display: "flex"}}>
+            <Logo />
+        </Toolbar>
+    </Appbar>
   )
 }
 
